5358f03037a8e36d601900656e24d6975a373ae4
Nicholas Ignoffo <nick@tehnut.info>
2018-11-03 16:20:20 -0700

    Theme switcher :zoomeyes:

--- js/settings.js
+++ js/settings.js
@@ -1,7 +1,10 @@
 document.addEventListener("DOMContentLoaded", e => {
   chrome.storage.local.get({ user_info: { name: "", id: 0, site_url: "" } }, value => {
-    let loginText = "Logged in as <a href='" + value.user_info.site_url + "' style='color:rgb(var(--color-text-bright));font-weight:bold;' target='_blank'>" + value.user_info.name + "</a>";
+    let loginText = "Logged in as <a href='" + value.user_info.site_url + "' style='color:rgb(var(--color-blue));font_weight:bold;' target='_blank'>" + value.user_info.name + "</a>";
     document.getElementById("login-name").insertAdjacentHTML("afterbegin", loginText);
+
+    document.getElementById("theme-light").addEventListener("click", () => setTheme("light"));
+    document.getElementById("theme-dark").addEventListener("click", () => setTheme("dark"));
 
     document.getElementById("logout-button").addEventListener("click", () => {
       chrome.storage.local.remove(["access_token", "refresh_token", "user_info"], value => {});
