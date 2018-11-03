e9c360f71cee4dfbf796561e679a66219dce4737
Nicholas Ignoffo <nick@tehnut.info>
2018-11-02 20:52:14 -0700

    Some refactoring

---
+++ js/media_list.js
@@ -0,0 +1,72 @@
+const anilistCall = (query, variables, token) =>
+  fetch('https://graphql.anilist.co', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      Authorization: "Bearer " + token
+    },
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  });
+
+  function handleList(userId, token) {
+    anilistCall(anilistQuery, {
+        user: userId
+      }, token)
+      .then(res => res.json())
+      .then(res => {console.log(res); return res.data})
+      .then(res => {
+        let animeEntries = res.anime.lists[0].entries;
+        handleEntries("airing-anime", animeEntries.filter(entry => entry.media.nextAiringEpisode), (o1, o2) => o1.media.nextAiringEpisode.timeUntilAiring - o2.media.nextAiringEpisode.timeUntilAiring);
+        handleEntries("anime", animeEntries.filter(entry => !entry.media.nextAiringEpisode), (o1, o2) => o2.updatedAt - o1.updatedAt);
+        handleEntries("manga", res.manga.lists[0].entries, (o1, o2) => o2.updatedAt - o1.updatedAt);
+      });
+  }
+
+  function handleEntries(listType, list, sortFunction) {
+    list.sort(sortFunction);
+    for (let media in list.slice(0, 20)) {
+      let entry = list[media];
+      media = entry.media;
+      document.getElementById(listType).insertAdjacentHTML('beforeend', getHtml(media, entry.progress));
+    }
+  }
+
+  function getHtml(media, progress) {
+    let addedContent = "";
+    let ret = showHtml.replace("#{img}", media.coverImage.large).replace("#{is_airing}", media.nextAiringEpisode ? "initial" : "none").replace("#{site_url}", media.siteUrl);
+
+    addedContent += "<span style='display: none;'>" + media.title.romaji + "</span>";
+    if (media.nextAiringEpisode) {
+      let date = new Date(1970, 0, 1);
+      date.setSeconds(media.nextAiringEpisode.timeUntilAiring);
+      let isBehind = media.nextAiringEpisode.episode - 1 > progress;
+      addedContent += "<span class='overlay-text" + (isBehind ? " is-behind" : "") + "'>Ep " + media.nextAiringEpisode.episode + " - " + parseTime(media.nextAiringEpisode.timeUntilAiring) + "</span>"
+    }
+
+    return ret.replace("#{content}", addedContent);
+  }
+
+  function parseTime(secs) {
+    let seconds = parseInt(secs, 10);
+
+    let days = Math.floor(seconds / (3600 * 24));
+    seconds -= days * 3600 * 24;
+    let hours = Math.floor(seconds / 3600);
+    seconds -= hours * 3600;
+    let minutes = Math.floor(seconds / 60);
+    seconds -= minutes * 60;
+
+    let ret = "";
+    if (days > 0)
+      ret += days + "d";
+    if (hours > 0)
+      ret += (ret.length == 0 ? "" : " ") + hours + "h";
+    if (minutes > 0)
+      ret += (ret.length == 0 ? "" : " ") + minutes + "m"
+
+    return ret;
+  }
