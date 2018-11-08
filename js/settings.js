726a4261b69e22628a195febe09822449019f2f7
Nicholas Ignoffo <nick@tehnut.info>
2018-11-07 19:44:07 -0800

    Name in settings should use accent color

--- js/settings.js
+++ js/settings.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", e => {
   chrome.storage.local.get({ user_info: { name: "", id: 0, site_url: "" } }, value => {
-    let loginText = "Logged in as <a href='" + value.user_info.site_url + "' style='color:rgb(var(--color-blue));font_weight:bold;' target='_blank'>" + value.user_info.name + "</a>";
+    let loginText = "Logged in as <a href='" + value.user_info.site_url + "' style='color:rgb(var(--color-accent));font_weight:bold;' target='_blank'>" + value.user_info.name + "</a>";
     document.getElementById("login-name").insertAdjacentHTML("afterbegin", loginText);
 
     document.getElementById("theme-light").addEventListener("click", () => changeTheme("light"));
