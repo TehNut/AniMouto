7373dc8690828700a34eaa6971694b9101ff2369
Nicholas Ignoffo <nick@tehnut.info>
2019-04-30 18:00:56 -0700

    Add a button to refresh account information
    primarily for username and avatar

--- src/popup/components/settings/section/AccountSection.vue
+++ src/popup/components/settings/section/AccountSection.vue
@@ -4,13 +4,15 @@
     <div class="section">
       <h2 class="title">Logged in as <a :href="user.url" class="highlight">{{ user.name }}</a></h2>
       <div class="button no-select ripple" @click="logout">Logout</div>
+      <div class="button no-select ripple" @click="updateUser">Refresh User</div>
       <p style="margin:0">Remember to revoke the access token from your <a href="https://anilist.co/settings/apps">Apps page</a> after logging out.</p>
     </div>
   </div>
 </template>
 
 <script>
-  import {updateUser} from "../../../../assets/js/utils";
+  import {updateUser, queryAL} from "../../../../assets/js/utils";
+  import viewerQuery from "../../../../assets/graphql/viewer.graphql";
 
   export default {
     name: "AccountSection",
@@ -29,6 +31,23 @@
         this.$emit("update-theme");
         updateUser();
         this.$emit("add-toast", { message: "Logout successful" })
+      },
+      updateUser() {
+        const _self = this;
+        this.$browser.storage.local.get().then(v => {
+          if (v.access_token === "") {
+            this.logout();
+            return;
+          }
+
+          queryAL(viewerQuery, {}, v.access_token).then(res => res.data.Viewer).then(viewer => {
+            this.$browser.storage.local.set({ user_info: { name: viewer.name, id: viewer.id, site_url: viewer.siteUrl, avatar: viewer.avatar.large } }).then(v => {
+              updateUser();
+              _self.user.name = v.user_info.name;
+              _self.user.url = v.user_info.site_url;
+            });
+          });
+        })
       }
     },
     created() {
