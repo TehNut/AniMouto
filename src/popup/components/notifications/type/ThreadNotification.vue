10f8f399768411ac5414d061d822ac1490fd68cc
Nicholas Ignoffo <nick@tehnut.info>
2019-05-06 18:05:24 -0700

    Fix support for ThreadLike notifications

--- src/popup/components/notifications/type/ThreadNotification.vue
+++ src/popup/components/notifications/type/ThreadNotification.vue
@@ -3,7 +3,7 @@
     <a :href="notification.user.url" target="_blank">
       <img class="avatar notification-icon no-select" :src="notification.user.img.large">
     </a>
-    <a :href="notification.thread.url + '/comment/' + notification.commentId" class="notification-body" target="_blank">
+    <a :href="notification.thread.url + (notification.commentId ? '/comment/' + notification.commentId : '')" class="notification-body" target="_blank">
       <span class="highlight">{{ notification.user.name }}</span> {{ notification.context }} <span class="highlight">{{ notification.thread.title }}</span>
     </a>
   </span>
