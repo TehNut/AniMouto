11f2f47f892afa35178f01c930128d5f1e2442f7
Nicholas Ignoffo <nick@tehnut.info>
2019-05-15 18:21:41 -0700

    Allow components to transform response data

--- src/popup/components/base/QueryContainer.vue
+++ src/popup/components/base/QueryContainer.vue
@@ -6,6 +6,9 @@
     <transition name="fade">
       <Error v-if="error">
         {{ errorText }}
+        <span v-if="typeof error !== 'boolean'">
+          {{ JSON.stringify(error) }}
+        </span>
       </Error>
     </transition>
     <transition name="fade">
@@ -30,6 +33,9 @@
       query: {
         type: Function // Must return a promise
       },
+      responsifier: {
+        type: Function
+      },
       errorText: {
         type: String
       }
@@ -37,11 +43,14 @@
     methods: {
       runQuery() {
         // First clear out any prior data in case of a refresh
-        this.response = null;
-        this.error = false;
+        this.reset();
 
         const _self = this;
-        this.query().then(res => _self.response = res).catch(err => _self.error = true);
+        this.query().then(res => _self.response = this.responsifier ? this.responsifier(res) : res).catch(err => _self.error = true);
+      },
+      reset() {
+        this.response = null;
+        this.error = false;
       }
     },
     created() {
